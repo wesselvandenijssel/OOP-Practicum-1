3328092d030bad697b4c71234a95e82b9efa0000
Wessel van den IJssel <wessel@mbbma.nl>
2023-11-16 14:20:27 +0100

    Loop

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Using a for loop to iterate from 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i); // Printing the current number
+        }
+    }
+}
